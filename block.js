ba052a606921d3da324cfeefb8fa92532125b716
“Gorgutzz” <leo.kalshteyn@gmail.com>
2021-07-12 20:47:26 -0700

    added block and block test

---
+++ block.js
@@ -0,0 +1,12 @@
+class Block {
+  constructor(timestamp, lastHash, hash, data) {
+    this.timestamp = timestamp;
+    this.lastHash = lastHash;
+    this.hash = hash;
+    this.data = data;
+  }
+}
+
+const block1 = new Block();
+
+module.exports = Block;
