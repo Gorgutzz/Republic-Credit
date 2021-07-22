37ed952d2a3ebbf395ce95445f8c205227a12388
“Gorgutzz” <leo.kalshteyn@gmail.com>
2021-07-21 20:42:38 -0700

    add mine block to test

--- block.test.js
+++ block.test.js
@@ -18,12 +18,24 @@ describe('Block', () => {
   describe('genesis()', () => {
     const genesisBlock = Block.genesis();
 
+    console.log('genesisBlock', genesisBlock);
+
     it('returns a Block instance', () => {
       expect(genesisBlock instanceof Block).toBe(true);
     });
 
     it('returns the genesis data', () => {
       expect(genesisBlock).toEqual(GENESIS_DATA);
+    });
+  });
+
+  describe('mineBlock()', () => {
+    const lastBlock = Block.genesis();
+    const data = 'mined data';
+    const mineBlock = Block.mineBlock({ lastBlock, data });
+
+    it('returns a Block instance', () => {
+      expect(minedBlock instanceof Block).toBe(true);
     });
   });
 });
