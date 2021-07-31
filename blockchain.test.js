0206a7abd070de7013a54cf67d645613e90ab493
“Gorgutzz” <leo.kalshteyn@gmail.com>
2021-07-31 14:59:03 -0700

    add more test cases to blockchain test

--- blockchain.test.js
+++ blockchain.test.js
@@ -18,4 +18,30 @@ describe('Blockchain', () => {
 
     expect(blockchain.chain[blockchain.chain.length-1].data).toEqual(newData);
   });
+
+  describe('isValidChain()', () => {
+    describe('when the chain does not start with the genesis block', () => {
+      it('returns false', () => {
+        blockchain.chain[0] = { data: 'fake-genesis' };
+
+        expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+      });
+    });
+
+    describe('when the chain starts with the genesis block and has multiple blocks', () => {
+      describe('and a lasthash reference has changed', () => {
+        it('returns false', () => {});
+      });
+
+      describe('and the chain contains a block with an invalid field', () => {
+        it('returns false', () => {});
+      });
+
+      describe('and the chain does not contain any invalid field', () => {
+        it('returns true', () => {
+
+        });
+      });
+    });
+  });
 });
