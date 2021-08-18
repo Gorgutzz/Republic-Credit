d12dd33803435083a87a92948a0962e9d452d893
“Gorgutzz” <leo.kalshteyn@gmail.com>
2021-08-18 16:30:46 -0700

    add average work script

---
+++ average-work.js
@@ -0,0 +1,24 @@
+const Blockchain = require('./blockchain');
+
+const blockchain = new Blockchain();
+
+blockchain.addBlock({ data: 'initial' });
+
+let prevTimestamp, nextTimestamp, nextBlock, timeDiff, average;
+
+const times = [];
+
+for (let i=0; i<10000; i++) {
+  prevTimestamp = blockchain.chain[blockchain.chain.length-1].timestamp;
+
+  blockchain.addBlock({ data: 'block ${i}'});
+  nextBlock = blockchain.chain[blockchain.chain.length-1];
+
+  nextTmestamp = nextBlock.timestamp;
+  timeDiff = nextTimestamp - prevTimestamp;
+  times.push(timeDiff);
+
+  average = time.reduce((total, num) => (total + num))/times.length;
+
+  console.log(`Time to mine block: ${timeDiff}ms. Difficult: ${nextBlock.difficulty}. Average time: ${average}ms`);
+}
