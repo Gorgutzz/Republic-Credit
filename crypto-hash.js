74162cf5889b72b242c293655b760d1a012c3d08
“Gorgutzz” <leo.kalshteyn@gmail.com>
2021-08-28 14:59:03 -0700

    installed redis

--- crypto-hash.js
+++ crypto-hash.js
@@ -1,6 +1,6 @@
 const crypto = require('crypto');
 
-const cryptohash = (...inputs) => {
+const cryptoHash = (...inputs) => {
   const hash = crypto.createHash('sha256');
 
   hash.update(inputs.sort().join(' '));
