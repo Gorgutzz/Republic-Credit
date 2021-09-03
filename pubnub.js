3eb4c8eb0c4d4e152c133a5455a4fa7aaf85a957
“Gorgutzz” <leo.kalshteyn@gmail.com>
2021-09-03 16:31:20 -0700

    added test log in pubnub

--- pubnub.js
+++ pubnub.js
@@ -15,5 +15,26 @@ class PubSub {
     this.pubnub = new PubNub(credentials);
 
     this.pubnub.subscribe({ channels: [Object.values(CHANNELS)] });
+
+    this.pubnub.addListener(this.listener());
+  }
+
+  listener() {
+    return {
+      message: messageObject => {
+        const { channel, message } = messageObject;
+
+        console.log(`Message recieved, Channel: ${channel}, Message: ${message}`);
+      }
+    };
+  }
+
+  publish({ channel, message }) {
+    this.pubnub.publish({ channel, message });
   }
 }
+
+const testPubSub = new PubSub();
+testPubSub.publish({ channel: CHANNELS.TEST, message: 'hello pubnub' });
+
+module.exports = PubSub;
