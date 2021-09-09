0b618a4d97e0f0c37717664c9bc87c01cb1416b3
“Gorgutzz” <leo.kalshteyn@gmail.com>
2021-09-08 17:15:54 -0700

    removed some redundant code

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ const pubsub = new PubSub({ blockchain });
 const DEFAULT_PORT = 3000;
 const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
 
-setTimeout(() => pubsub.broadcastChain(), 1000);
-
 app.use(bodyParser.json());
 
 app.get('/api/blocks', (req, res) => {
@@ -40,15 +38,17 @@ const syncChains = () => {
   });
 };
 
-const DEFAULT_PORT = 3000;
 let PEER_PORT;
 
 if (process.env.GENERATE_PEER_PORT === 'true') {
-  PEER_PORT = DEFAULT_PORT + Math.cell(Math.random() * 1000)
+  PEER_PORT = DEFAULT_PORT + Math.cell(Math.random() * 1000);
 }
 
 const PORT = PEER_PORT || DEFAULT_PORT;
 app.listen(PORT, () => {
   console.log(`listening at localhost:${PORT}`);
+
+  if (PORT !== DEFAULT_PORT) {
   syncChains();
+  }
 });
