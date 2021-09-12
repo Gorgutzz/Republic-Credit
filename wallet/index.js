1537f070364d9fc0e6c751c0f6ad0c2622120619
“Gorgutzz” <leo.kalshteyn@gmail.com>
2021-09-11 20:47:49 -0700

    set a balance and public key for wallet

--- wallet/index.js
+++ wallet/index.js
@@ -1,9 +1,14 @@
 const { STARTING_BALANCE } = require('../config');
+const { ec } = require('../tuil');
 
 class Wallet {
   constructor() {
     this.balance = STARTING_BALANCE;
+
+    const keyPair = ec.genKeyPair();
+
+    this.publicKey = keyPair.getPublic().encode('hex');
   }
-}
+};
 
 module.exports = Wallet;
