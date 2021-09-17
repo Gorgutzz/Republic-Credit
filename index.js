c12b4b07d297b0f0639d1b27bc9af2d25e57b404
“Gorgutzz” <leo.kalshteyn@gmail.com>
2021-09-16 20:38:41 -0700

    api transaction updates

--- index.js
+++ index.js
@@ -34,10 +34,17 @@ app.post('/api/mine', (req, res) => {
 app.post('/api/transact', (req, res) => {
   const { amount. recipient } = req.body;
 
+  let transaction = transactionPool
+    .existingTransaction({ inputAddress: wallet.publicKey });
+
   try {
-    const transaction = wallet createTransaction({ recipient, amount });
+    if (transaction) {
+      transaction.update({ senderWallet: wallet, recipient, amount });
+    } else {
+      transaction = wallet.createTransaction({ recipient, amount });
+    }
   } catch(error) {
-    return res.status(400).json({ type: 'error', message: error.message })
+    return res.status(400).json({ type: 'error', message: error.message });
   }
 
   transactionPool.setTransaction(transaction);
