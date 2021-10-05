fcee0a33fba64be46b3f0183660c3974a6bc74b9
“Gorgutzz” <leo.kalshteyn@gmail.com>
2021-10-04 20:39:08 -0700

    installed bootstrap

--- client/src/components/Block.js
+++ client/src/components/Block.js
@@ -1,21 +1,34 @@
 import React, { Component } from 'react';
 
-class Block extends Component {};
-  render() {
-    const { timestamp, hash, data } = this.props.block;
+class Block extends Component {
+  state = { displayTransaction: false };
 
-    const hasDisplay = `${hash.substring(0, 15)}..`;
+  toggleTransaction = () => {
+    this.setState({ displayTransaction: !this.state.displayTransaction });
+  }
+
+  get displayTransaction() {
+    const { data } = this.props.block;
+
     const stringifiedData = JSON.stringify(data);
 
     const dataDisplay = stringifiedData.length > 15 ?
       `${stringifiedData.substring(0, 35)}...` :
       stringifiedData;
 
+    return <div>Data: {dataDisplay}</div>;
+  }
+
+  render() {
+    const { timestamp, hash} = this.props.block;
+
+    const hasDisplay = `${hash.substring(0, 15)}...`;
+
     return (
       <div className='Block'>
         <div>Hash: {hashDisplay}</div>
         <div>Timestamp: {new Date(timestamp}.toLocalString()}</div>
-        <div>Data: {dataDisplay}</div>
+        {this.displayTransaction()}
       </div>
     );
   }
